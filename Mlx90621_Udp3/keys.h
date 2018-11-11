64699b313defe51e4444ac4ab2bd5f336bfd3dc6
Raman Raja <ramanraja@users.noreply.github.com>
2018-11-11 19:11:05 +0530

    OTA enabled

---
+++ Mlx90621_Udp3/keys.h
@@ -0,0 +1,17 @@
+// keys.h
+ 
+
+#ifndef KEYS_H
+#define KEYS_H
+
+#define  WIFI_SSID1         "fghghf"
+#define  WIFI_PASSWORD1     "fghfgh"
+
+#define  WIFI_SSID2         "hgdjgg"
+#define  WIFI_PASSWORD2     "fghghg"
+ 
+#define  WIFI_SSID3         ""
+#define  WIFI_PASSWORD3     ""
+ 
+
+#endif 
